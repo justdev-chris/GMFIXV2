9d2ea9ce9d22db2d9c1f6c32dfbd9e8bcfeccba5
Geometry Tryhard <memelite69@gmail.com>
2024-11-06 18:31:13 -0500

    update

--- js/style.js
+++ js/style.js
@@ -12,12 +12,16 @@ function styleload() {
     var storage = localStorage.getItem('style');
     document.getElementById('style').href = storage;
    if (storage === null){
-       localStorage.setItem('style', 'css/style.css');
+       localStorage.setItem('style', 'css/style-cyan.css');
    styleload()
    }
  
 }
-
+var head = document.getElementsByTagName('head')[0];
+var style = document.createElement('link');
+style.rel = 'stylesheet';
+style.href = 'css/global.css';
+head.appendChild(style);
 
 function switchstyles(){
  var current = localStorage.getItem('style');

ae02a7ecb51e82a3252f4d06c090a4a94944e0e9
Geometry Tryhard <memelite69@gmail.com>
2024-11-10 19:48:35 -0500

    update

--- assets/chicken-merge/patch/js/poki-master-loader.js
+++ assets/chicken-merge/patch/js/poki-master-loader.js
@@ -22,14 +22,14 @@ if (!window.config.unityWebglLoaderUrl) {
       , minor = versionSplit[1];
     switch (year) {
     case "2019":
-        window.config.unityWebglLoaderUrl = 1 === minor ? "patch/js/UnityLoader.2019.1.js" : "patch/js/UnityLoader.2019.2.js";
+        window.config.unityWebglLoaderUrl = 1 === minor ? "js/unity/UnityLoader.2019.1.js" : "js/unity/UnityLoader.2019.2.js";
         break;
     default:
-        window.config.unityWebglLoaderUrl = "patch/js/UnityLoader.js"
+        window.config.unityWebglLoaderUrl = "js/unity/UnityLoader.js"
     }
 }
 var sdkScript = document.createElement("script");
-sdkScript.src = "patch/js/poki-sdk.js",
+sdkScript.src = "patch/js/poki-sdk-v2.js",
 sdkScript.onload = function() {
     var i = document.createElement("script");
     i.src = root + loader,

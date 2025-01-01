03de20666d223a67a6f7781d5691e7fa38c19651
Geometry Tryhard <memelite69@gmail.com>
2024-12-31 22:40:06 -0500

    update

--- js/gamesloader.js
+++ js/gamesloader.js
@@ -8,7 +8,7 @@ function launchIframe(contentURL) {
     if (element !== null) {
 element.src = contentURL;
         localStorage.setItem('cloakurl', element.src);
-
+        element.contentWindow.focus();
         element.scrollIntoView({ 
           behavior: "smooth" 
         }); 
@@ -43,7 +43,6 @@ function launchGame(contentURL){
     frame.style = `background: #000000; max-width: 72vh; min-width: 960px; max-height: 40.5vh; min-height: 540px; border: none;`;
     frame.src = contentURL;
     frame.id = 'gameframe_';
-frame.contentWindow.focus();
     //add frame to div for frame
     frameDiv.appendChild(frame);
     //add div for frame to center
